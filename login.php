d3f7178f010575d022990d27601792d58ab2e3d9
rotsendiego <rotsen.d.albuquerque@accenture.com>
2017-01-11 16:53:27 -0200

    atualizado
    atualização tela de precadastro e login

--- login.php
+++ login.php
@@ -1,126 +1,37 @@
-<!-- All the files that are required -->
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-<link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
 
 <link href='css/login.css' rel='stylesheet' type='text/css'>
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/bootstrap-responsive.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+ <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:400,700">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Serif">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Boogaloo">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Economica:700,400italic">
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script>
-<script src="js/login.css"></script>
-
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-
-<!-- Where all the magic happens -->
-<!-- LOGIN FORM -->
-<div class="text-center" style="padding:50px 0" align="center">
-	<div class="logo">login</div>
-	<!-- Main Form -->
-	<div class="login-form-1">
-		<form id="login-form" class="text-left">
-			<div class="login-form-main-message"></div>
-			<div class="main-login-form">
-				<div class="login-group">
-					<div class="form-group">
-						<label for="lg_username" class="sr-only">Username</label>
-						<input type="text" class="form-control" id="lg_username" name="lg_username" placeholder="username">
-					</div>
-					<div class="form-group">
-						<label for="lg_password" class="sr-only">Password</label>
-						<input type="password" class="form-control" id="lg_password" name="lg_password" placeholder="password">
-					</div>
-					<div class="form-group login-group-checkbox">
-						<input type="checkbox" id="lg_remember" name="lg_remember">
-						<label for="lg_remember">remember</label>
-					</div>
-				</div>
-				<button type="submit" class="login-button"><i class="fa fa-chevron-right"></i></button>
-			</div>
-			<div class="etc-login-form">
-				<p>forgot your password? <a href="#">click here</a></p>
-				<p>new user? <a href="#">create new account</a></p>
-			</div>
-		</form>
-	</div>
-	<!-- end:Main Form -->
-</div>
-
-<!-- REGISTRATION FORM -->
-<div class="text-center" style="padding:50px 0">
-	<div class="logo">register</div>
-	<!-- Main Form -->
-	<div class="login-form-1">
-		<form id="register-form" class="text-left">
-			<div class="login-form-main-message"></div>
-			<div class="main-login-form">
-				<div class="login-group">
-					<div class="form-group">
-						<label for="reg_username" class="sr-only">Email address</label>
-						<input type="text" class="form-control" id="reg_username" name="reg_username" placeholder="username">
-					</div>
-					<div class="form-group">
-						<label for="reg_password" class="sr-only">Password</label>
-						<input type="password" class="form-control" id="reg_password" name="reg_password" placeholder="password">
-					</div>
-					<div class="form-group">
-						<label for="reg_password_confirm" class="sr-only">Password Confirm</label>
-						<input type="password" class="form-control" id="reg_password_confirm" name="reg_password_confirm" placeholder="confirm password">
-					</div>
-					
-					<div class="form-group">
-						<label for="reg_email" class="sr-only">Email</label>
-						<input type="text" class="form-control" id="reg_email" name="reg_email" placeholder="email">
-					</div>
-					<div class="form-group">
-						<label for="reg_fullname" class="sr-only">Full Name</label>
-						<input type="text" class="form-control" id="reg_fullname" name="reg_fullname" placeholder="full name">
-					</div>
-					
-					<div class="form-group login-group-checkbox">
-						<input type="radio" class="" name="reg_gender" id="male" placeholder="username">
-						<label for="male">male</label>
-						
-						<input type="radio" class="" name="reg_gender" id="female" placeholder="username">
-						<label for="female">female</label>
-					</div>
-					
-					<div class="form-group login-group-checkbox">
-						<input type="checkbox" class="" id="reg_agree" name="reg_agree">
-						<label for="reg_agree">i agree with <a href="#">terms</a></label>
-					</div>
-				</div>
-				<button type="submit" class="login-button"><i class="fa fa-chevron-right"></i></button>
-			</div>
-			<div class="etc-login-form">
-				<p>already have an account? <a href="#">login here</a></p>
-			</div>
-		</form>
-	</div>
-	<!-- end:Main Form -->
-</div>
-
-<!-- FORGOT PASSWORD FORM -->
-<div class="text-center" style="padding:50px 0">
-	<div class="logo">forgot password</div>
-	<!-- Main Form -->
-	<div class="login-form-1">
-		<form id="forgot-password-form" class="text-left">
-			<div class="etc-login-form">
-				<p>When you fill in your registered email address, you will be sent instructions on how to reset your password.</p>
-			</div>
-			<div class="login-form-main-message"></div>
-			<div class="main-login-form">
-				<div class="login-group">
-					<div class="form-group">
-						<label for="fp_email" class="sr-only">Email address</label>
-						<input type="text" class="form-control" id="fp_email" name="fp_email" placeholder="email address">
-					</div>
-				</div>
-				<button type="submit" class="login-button"><i class="fa fa-chevron-right"></i></button>
-			</div>
-			<div class="etc-login-form">
-				<p>already have an account? <a href="#">login here</a></p>
-				<p>new user? <a href="#">create new account</a></p>
-			</div>
-		</form>
-	</div>
-	<!-- end:Main Form -->
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 col-md-4 col-md-offset-4">
+            <h2 class="text-center login-title">Bem Vindo Ao nosso Portal</h2>
+            <div class="account-wall">
+	            <div class="image-login">
+	            	<img  height="320" width="300" src="img/algoritech_logo.jpeg">
+	            </div>
+                <form class="form-signin">
+                <input style="width:330;" type="text" class="form-control" placeholder="Email" required autofocus>
+                <input style="width:330;"" type="password" class="form-control" placeholder="Senha" required>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">
+                    Entrar</button>
+                <label class="checkbox pull-left">
+                    <input type="checkbox" value="remember-me">
+                    Lembrar
+                </label>
+                <a href="#" class="pull-right need-help">Ajuda</a><span class="clearfix"></span>
+                </form>
+            </div>
+            <a href="portal-algoritech/precadastro.php" class="text-center new-account">Crie um cadastro</a>
+        </div>
+    </div>
 </div>
---
+++ precadastro.php
@@ -0,0 +1,78 @@
+<?php
+	include ('header.php');
+?>
+		
+
+
+		<!--
+		</div> end: Container  -->	
+
+		<!-- start: Container -->	
+		<div class="container">
+
+			<!-- start: Contact Form -->
+			<div class="span4">
+				<div class="title"><h2>Cadastre-se</h2></div>
+				<br>
+
+				<!-- start: Contact Form -->
+				<div id="contact-form">
+					
+					<form method="post" action="###">
+						
+						<fieldset>
+							<div class="clearfix">
+								<label for="name"><span>Nome Completo:</span></label>
+								<div class="input">
+									<input tabindex="1" size="18" id="name" name="name" type="text" value="">
+								</div>
+							</div>
+
+							<div class="clearfix">
+								<label for="email"><span>Login (Email):</span></label>
+								<div class="input">
+									<input tabindex="2" size="25" id="email" name="email" type="text" value="" class="input-xlarge">
+								</div>
+							</div>
+
+
+							<div class="clearfix">
+								<label for="email"><span>Confirmar Login (Email):</span></label>
+								<div class="input">
+									<input tabindex="2" size="25" id="email2" name="email" type="text" value="" class="input-xlarge">
+								</div>
+							</div>
+
+							<div class="clearfix">
+								<label for="email"><span>Senha</span></label>
+								<div class="input">
+									<input tabindex="2" size="8" id="senha" name="senha" type="password" value="" class="input-xlarge">
+								</div>
+							</div>
+
+								<div class="clearfix">
+								<label for="email"><span>Confirmar Senha</span></label>
+								<div class="input">
+									<input tabindex="2" size="8" id="senha2" name="senha" type="password" value="" class="input-xlarge">
+								</div>
+							</div>
+							 <label class="checkbox pull-left">
+                    	<input type="checkbox" value="termos-de-uso">Sim, eu concordo com os 
+						<a href="#"><b>Termos de uso</b></a> </label>
+
+							<div class="actions">
+								<button tabindex="3" type="submit" class="btn btn-succes btn-large">Cadastrar</button>
+							</div>
+						</fieldset>
+					
+					</form>
+					
+				</div>
+				<!-- end: Contact Form -->
+
+
+
+<?php
+	include ('footer.php');
+?>
+		
