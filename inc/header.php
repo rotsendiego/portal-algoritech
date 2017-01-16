44d06718c4cc2d7b05f644b34b21fbaac5874b82
rotsendiego <rotsen.d.albuquerque@accenture.com>
2017-01-16 16:55:27 -0200

    cabeçalho
    novo menu

--- inc/header.php
+++ inc/header.php
@@ -23,9 +23,9 @@
 	<!-- end: Facebook Open Graph -->
 
     <!-- start: CSS -->
-    <link href="../css/bootstrap.css" rel="stylesheet">
-    <link href="../css/bootstrap-responsive.css" rel="stylesheet">
-	<link href="../css/style.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:400,700">
 	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Serif">
 	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Boogaloo">
@@ -84,7 +84,7 @@
 										</div>
 									</div>
 								</li>
-								<!--<li>
+								<li>
 									<div class="social-small-item">				
 										<div class="social-small-info-wrap">
 											<div class="social-small-info">
@@ -97,7 +97,7 @@
 											</div>
 										</div>
 									</div>
-								</li> -->
+								</li>
 								<li>
 									<div class="social-small-item">				
 										<div class="social-small-info-wrap">
@@ -134,20 +134,47 @@
           				</a>
           				<div class="nav-collapse collapse">
             				<ul class="nav">
-              					<li class="active"><a href="../index.php">Início</a></li>
-              					<li><a href="#">Sobre Algoritech</a></li>
-								
-              					<li><a href="../contact.php">Contato</a></li>
-              					<li><a href="../precadastro.php">Cadastre-se</a></li>
-              					<li class="dropdown">
-                					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Serviços<b class="caret"></b></a>
+              					<li class="active"><a href="index.php">Início</a></li>
+
+
+              				<li class="dropdown">
+                					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Usuário <b class="caret"></b></a>
                 					<ul class="dropdown-menu">
-                  						<li><a href="#">Teste 1</a></li>
-                  						<li class="divider"></li>
-                  						<li class="nav-header">Título</li>
-                  						<li><a href="#">Teste 2</a></li>
+                  						<li><a href="#">Cadastrar Usuário</a></li>
+                  						<li><a href="#">Listar Usuário</a></li>
+                  						
                 					</ul>
               					</li>
+
+
+              				<li class="dropdown">
+                					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Cargo do Contato  <b class="caret"></b></a>
+                					<ul class="dropdown-menu">
+                  						<li><a href="#">Cadastrar Contato</a></li>
+                  						<li><a href="#">Listar Contato</a></li>
+                					</ul>
+              					</li>
+
+
+              				<li class="dropdown">
+                					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Categoria do Cliente <b class="caret"></b></a>
+                					<ul class="dropdown-menu">
+                  						<li><a href="#">Cadastrar Categoria</a></li>
+                  						<li><a href="#">Listar Categoria</a></li>
+                  						
+                					</ul>
+              					</li>
+
+
+              				<li class="dropdown">
+                					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Relatório <b class="caret"></b></a>
+                					<ul class="dropdown-menu">
+                  						<li><a href="#">Relatórido Geral</a></li>
+                  						<li><a href="#">Relatório por Usuário</a></li>
+                  							
+                					</ul>
+              					</li>
+
             				</ul>
           				</div>
         			</div>
